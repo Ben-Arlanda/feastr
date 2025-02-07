ca1a813fda9fb39366efac0fd0cd62cacfa9e5bf
Ben-Arlanda <ben.arlanda@protonmail.com>
2025-02-07 13:27:21 +1100

    added api call with axios and added biome config

--- app/api/mealAPI.ts
+++ app/api/mealAPI.ts
@@ -1,29 +1,44 @@
-import axios from "axios";
+import axios from 'axios';
 
 const API_KEY = process.env.EXPO_PUBLIC_API_KEY as string;
 
 const BASE_URL = process.env.EXPO_PUBLIC_API_URL as string;
 
 export const MealsByMood = async (mood: string) => {
-	let query = "";
+  let query = '';
 
-	switch (mood) {
-		case "happy":
-			query = "healthy";
-			break;
-		case "sad":
-			query = "comfort food";
-			break;
-		case "tired":
-			query = "energy";
-			break;
-		case "stressed":
-			query = "calming";
-			break;
-		case "excited":
-			query = "spicy";
-			break;
-		default:
-			query = "popular";
-	}
+  switch (mood) {
+    case 'happy':
+      query = 'healthy';
+      break;
+    case 'sad':
+      query = 'comfort food';
+      break;
+    case 'tired':
+      query = 'energy';
+      break;
+    case 'stressed':
+      query = 'calming';
+      break;
+    case 'excited':
+      query = 'spicy';
+      break;
+    default:
+      query = 'popular';
+  }
+
+  try {
+    const response = await axios.get(`${BASE_URL}/complexSearch`, {
+      params: {
+        query,
+        number: 3,
+        apiKey: API_KEY,
+      },
+    });
+
+    return response.data.results;
+  } catch (error) {
+    console.error('Error fetching meals:', error);
+    return [];
+  }
 };
