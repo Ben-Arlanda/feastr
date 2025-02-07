bae7aaff8ba06af8572384ce1d4d3a821bfbd547
Ben-Arlanda <ben.arlanda@protonmail.com>
2025-02-07 13:12:21 +1100

    added swtich case to reference moods and query from api

--- app/api/mealAPI.ts
+++ app/api/mealAPI.ts
@@ -4,6 +4,26 @@ const API_KEY = process.env.EXPO_PUBLIC_API_KEY as string;
 
 const BASE_URL = process.env.EXPO_PUBLIC_API_URL as string;
 
-export const MealsByMood = async () => {
+export const MealsByMood = async (mood: string) => {
 	let query = "";
+
+	switch (mood) {
+		case "happy":
+			query = "healthy";
+			break;
+		case "sad":
+			query = "comfort food";
+			break;
+		case "tired":
+			query = "energy";
+			break;
+		case "stressed":
+			query = "calming";
+			break;
+		case "excited":
+			query = "spicy";
+			break;
+		default:
+			query = "popular";
+	}
 };
