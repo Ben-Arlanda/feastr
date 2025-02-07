77128d1dfd30a7eb510c30fac502fb9932351d7e
Ben-Arlanda <ben.arlanda@protonmail.com>
2025-02-07 12:46:24 +1100

    added env and created api directory to set up key and url

---
+++ app/api/mealAPI.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const API_KEY = process.env.EXPO_PUBLIC_API_KEY as string;
+
+const BASE_URL = process.env.EXPO_PUBLIC_API_URL as string;
+
+export const MealsByMood = async () => {
+	let query = "";
+};
